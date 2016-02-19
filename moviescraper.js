2b58ae38a8d9e8402324f8295265b5b81d1924b5
Breandan Choi <breandanchoi@Jamess-MacBook-Pro.local>
2016-02-19 17:43:14 -0500

    Added Opening+Coming Soon

--- moviescraper.js
+++ moviescraper.js
@@ -2,8 +2,8 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	getCSV = require('./getcsv'),
 	fs = require('fs'),
-	csv = require('fast-csv');
-
+	csv = require('fast-csv'),
+	async = require('async');
 
 require('./date.js');
 var date = new Date().toString('M/d/yyyy');
@@ -18,7 +18,7 @@ var title,
 	movies_objects_array = [],
 	push = true;
 
-
+var array_movies = [];
 
 console.log(movies_url);
 
@@ -31,6 +31,8 @@ getCSV('New Movies.csv', function(err,data){
 				title = title.trim(' ');
 				console.log(title);
 
+				var title_for_showtime = title.replace(/ |:/g, '-');
+				array_movies.push(title_for_showtime); //store all the movie titles for finding the showtime
 
 				for(var i=0; i<data.length; i++){
 					if (data[i].title == title){
@@ -63,16 +65,27 @@ getCSV('New Movies.csv', function(err,data){
 				Length: 'Show Time'
 			});
 
-			var cineplex_movie_name = 'kung-fu-panda-3'
-			var cineplex_movie_url = 'http://www.cineplex.com/Showtimes/'+ cineplex_movie_name +'/cineplex-cinemas-yongeeglinton-and-vip-formerly-silvercity?Date=2/19/2016';
-			request(cineplex_movie_url, function(err,response,body){
-				var $ = cheerio.load(body);
-				$('.showtime--list li').each(function(index, items){
-					showtime = $(items).find('.showtime').text();
-					movies_objects_array.push({
-						Length: showtime
-					});
+			async.eachSeries(array_movies, function(item, next_movie){
+				var cineplex_movie_url = 'http://www.cineplex.com/Showtimes/'+ item +'/cineplex-cinemas-yongeeglinton-and-vip-formerly-silvercity?Date=2/19/2016';
+				console.log(cineplex_movie_url);
+				request(cineplex_movie_url, function(err,response,body){
+					if (!err && response.statusCode == 200){
+						var $ = cheerio.load(body);
+						$('.showtime--list li').each(function(index, items){
+							showtime = $(items).find('.showtime').text();
+							movies_objects_array.push({
+								Movie_Title: item,
+								Length: showtime
+							});
+						});
+						return next_movie();
+					} else {
+						console.log('We have encountered' + err);
+						return next_movie();
+					}
 				});
+			}, function(){
+				console.log('hi');
 				//Adding an empty row
 				movies_objects_array.push({
 					Movie_Title: '',
@@ -108,10 +121,11 @@ getCSV('New Movies.csv', function(err,data){
 						Movie_Title: '',
 						Length: ''
 					});
-					//Adding a row with heading Coming Soon and Date
+					//Adding a row with heading Opening This Week, Release Date, and Trailer
 					movies_objects_array.push({
-						Movie_Title: 'Coming Soon',
-						Length: 'Release Date'
+						Movie_Title: 'Opening + Coming Soon',
+						Length: 'Release Date',
+						Trailer: 'Trailer'
 					});
 
 					$('#homepage-opening-this-week .sidebarInTheaterOpening').each(function(index, items){
@@ -138,9 +152,29 @@ getCSV('New Movies.csv', function(err,data){
 });
 
 function write_file(){
-    var ws_new_movies = fs.createWriteStream('New Movies.csv');
-    csv.
-    write(movies_objects_array, {headers:true}).pipe(ws_new_movies);  
+
+	request(top_box_office_url, function(err, response, body){
+		if (!err && response.statusCode == 200){
+			var $ = cheerio.load(body);
+			$('#homepage-top-coming-soon .sidebarInTheaterOpening').each(function(index, items){
+				var movie = $(items).find('.middle_col').text();
+				var release_date = $(items).find('.right_col').text();
+				var trailer = 'https://www.youtube.com/results?search_query=' + movie;
+				movies_objects_array.push({
+					Movie_Title: movie,
+					Length: release_date,
+					Trailer: trailer
+				});
+			});
+			console.log(movies_objects_array);
+			var ws_new_movies = fs.createWriteStream('New Movies.csv');
+		    csv.
+		    write(movies_objects_array, {headers:true}).pipe(ws_new_movies);  
+		} else {
+			console.log('We have encountered' + err);
+		}
+	});
+
 }
 
 
